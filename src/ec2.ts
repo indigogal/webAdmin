71878440da52bf7600fed388e2c748e9d33f3863
megaratarex <santybe.1@hotmail.com>
2026-04-20 12:37:27 -0600

    corrección de funciones: se agregó el cliente a los parámetros

--- src/ec2.ts
+++ src/ec2.ts
@@ -13,15 +13,14 @@ import {
 import type { EC2ClientConfig } from "@aws-sdk/client-ec2";
 import 'dotenv/config'
 
-const config: EC2ClientConfig = {};
-const client = new EC2Client(config);
 
 /**
 Importa una llave para la creación de una instancia.
+ @param client{EC2Client} Cliente para enviar los comandos EC2.
  @param key {string} El contenido de la llave. Por defecto usa la llave SSH en el archivo de variables de entorno "SSH_KEY".
  @param name{string} El nombre asignado de la llave. Por defecto la nombra "my_key_pair"
  */
-async function ImportKeyPair(key: string = process.env.SSH_KEY, name: string = "my_key_pair"):Promise<ImportKeyPairCommandOutput> {
+async function ImportKeyPair(client: EC2Client, key: string = process.env.SSH_KEY, name: string = "my_key_pair"):Promise<ImportKeyPairCommandOutput> {
     const input: ImportKeyPairCommandInput = {
         TagSpecifications: [
             {
@@ -46,10 +45,11 @@ async function ImportKeyPair(key: string = process.env.SSH_KEY, name: string = "
 
 /**
  * Crea un grupo de seguridad vacío.
+ * @param client{EC2Client} Cliente para enviar los comandos EC2.
  * @param name{string} Nombre del grupo de seguridad.
  * @param description{string} Descripción del grupo de seguridad.
  */
-async function CreateSecurityGroup(name: string = "my_security_group", description: string = ""):Promise<CreateSecurityGroupResult>{
+async function CreateSecurityGroup(client: EC2Client, name: string = "my_security_group", description: string = ""):Promise<CreateSecurityGroupResult>{
     const input: CreateSecurityGroupCommandInput = {
         GroupName: name,
         Description: description,
@@ -61,12 +61,13 @@ async function CreateSecurityGroup(name: string = "my_security_group", descripti
 
 /**
  * Añade una IP a un grupo de seguridad existente.
+ * @param client{EC2Client} Cliente para enviar los comandos EC2.
  * @param groupId La ID del grupo de seguridad.
  * @param port El puerto que se desea abrir para el protocolo deseado
  * @param ip La IP a permitir. En caso de no ingresar algo, se obtiene la ip pública desde la ejecución del programa.
  * @param protocol El protocolo IP a permitir. Por defecto es TCP.
  */
-async function AllowIPForSG(groupId:string, port: number, ip: string = "", protocol: string = "tcp"): Promise<AuthorizeSecurityGroupIngressCommandOutput> {
+async function AllowIPForSG(client: EC2Client, groupId:string, port: number, ip: string = "", protocol: string = "tcp"): Promise<AuthorizeSecurityGroupIngressCommandOutput> {
     if (ip == "") {
         const res: Response = await fetch("http://checkip.amazonaws.com");
         ip = (await res.text()).trim();
@@ -90,6 +91,7 @@ async function AllowIPForSG(groupId:string, port: number, ip: string = "", proto
 
 /**
  * Crea una nueva instancia EC2.
+ * @param client{EC2Client} Cliente para enviar los comandos EC2.
  * @param iamProfile{IamInstanceProfileSpecification} Especificación del perfil AMI a usar en la instancia.
  * @param instanceType{_InstanceType} Enumerador string con el tipo de instancia a usar.
  * @param keyPath{string} Nombre de la llave SSH a usar.
@@ -99,7 +101,8 @@ async function AllowIPForSG(groupId:string, port: number, ip: string = "", proto
  * @param userData{string} Script de inicio para la instancia.
  *
  */
-async function CreateEC2Instance(iamProfile:IamInstanceProfileSpecification,
+async function CreateEC2Instance(client: EC2Client,
+                                 iamProfile:IamInstanceProfileSpecification,
                                  instanceType:_InstanceType,
                                  keyPath:string,
                                  securityGroupIds:[string],
